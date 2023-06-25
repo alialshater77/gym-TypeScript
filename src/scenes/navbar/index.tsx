ce9d460ac09c0eb043a08e8b4e2e1a556aa0ea6f
Ali Alshater <alialshater7@gmail.com>
2023-06-25 11:13:50 +0300

    SECOND commit

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect , useState  } from "react";
+import { useState  } from "react";
 import { Bars3Icon , XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/logo.png";
 import Link from "./Link";
